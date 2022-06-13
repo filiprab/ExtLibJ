a6978d46d9b48a1353f3084d92e151986456743d
pokkst <pokkst@samourai.is>
2022-06-13 15:19:28 -0500

    WIP: Forking some PaynymApi stuff over. Need to port APIFactory next.

--- java/com/samourai/wallet/api/paynym/PaynymApi.java
+++ java/com/samourai/wallet/api/paynym/PaynymApi.java
@@ -4,8 +4,12 @@ import com.samourai.http.client.IHttpClient;
 import com.samourai.wallet.api.backend.IBackendClient;
 import com.samourai.wallet.api.backend.beans.HttpException;
 import com.samourai.wallet.api.paynym.beans.*;
+import com.samourai.wallet.bip47.BIP47Meta;
 import com.samourai.wallet.bip47.BIP47UtilGeneric;
 import com.samourai.wallet.bip47.rpc.BIP47Wallet;
+import com.samourai.wallet.bip47.rpc.PaymentAddress;
+import com.samourai.wallet.bip47.rpc.PaymentCode;
+import com.samourai.wallet.bip47.rpc.java.Bip47UtilJava;
 import com.samourai.wallet.util.JSONUtils;
 import com.samourai.wallet.util.MessageSignUtilGeneric;
 import io.reactivex.Observable;
@@ -13,6 +17,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -153,6 +158,94 @@ public class PaynymApi {
             .map(responseOpt -> responseOpt.get());
   }
 
+  public void syncPcode(BIP47Wallet bip47Wallet, String pCode) {
+    try {
+      PaymentCode paymentCode = new PaymentCode(pCode);
+      int idx = 0;
+      boolean loop = true;
+      ArrayList<String> addrs = new ArrayList<>();
+      while (loop) {
+        addrs.clear();
+        for (int i = 0; i < idx + 20; i++) {
+          PaymentAddress sendAddress = Bip47UtilJava.getInstance().getSendAddress(bip47Wallet, paymentCode, i, bip47Wallet.getParams());
+          BIP47Meta.getInstance().getIdx4AddrLookup().put(Bip47UtilJava.getInstance().getSendPubKey(bip47Wallet, paymentCode, i, bip47Wallet.getParams()), i);
+          BIP47Meta.getInstance().getPCode4AddrLookup().put(Bip47UtilJava.getInstance().getSendPubKey(bip47Wallet, paymentCode, i, bip47Wallet.getParams()), paymentCode.toString());
+          addrs.add(Bip47UtilJava.getInstance().getSendPubKey(bip47Wallet, paymentCode, i, bip47Wallet.getParams()));
+        }
+
+        int nb = 0;
+        //val nb = APIFactory.getInstance().syncBIP47Outgoing(s)
+        //                        Log.i("BIP47Activity", "sync send idx:" + idx + ", nb == " + nb);
+        if (nb == 0) {
+          loop = false;
+        }
+        idx += 20;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+/*
+  public fun syncPcode(pcode: String) {
+    try {
+      val payment_code = PaymentCode(pcode)
+      var idx = 0
+      var loop = true
+      val addrs = ArrayList<String>()
+      while (loop) {
+        addrs.clear()
+        for (i in idx until idx + 20) {
+//                            Log.i("BIP47Activity", "sync receive from " + i + ":" + BIP47Util.getInstance(BIP47Activity.this).getReceivePubKey(payment_code, i));
+          BIP47Meta.getInstance().idx4AddrLookup[BIP47Util.getInstance(context).getReceivePubKey(payment_code, i)] = i
+          BIP47Meta.getInstance().pCode4AddrLookup[BIP47Util.getInstance(context).getReceivePubKey(payment_code, i)] = payment_code.toString()
+          addrs.add(BIP47Util.getInstance(context).getReceivePubKey(payment_code, i))
+          //                            Log.i("BIP47Activity", "p2pkh " + i + ":" + BIP47Util.getInstance(BIP47Activity.this).getReceiveAddress(payment_code, i).getReceiveECKey().toAddress(SamouraiWallet.getInstance().getCurrentNetworkParams()).toString());
+        }
+        val s = addrs.toTypedArray()
+        val nb = APIFactory.getInstance(context).syncBIP47Incoming(s)
+        //                        Log.i("BIP47Activity", "sync receive idx:" + idx + ", nb == " + nb);
+        if (nb == 0) {
+          loop = false
+        }
+        idx += 20
+      }
+      idx = 0
+      loop = true
+      BIP47Meta.getInstance().setOutgoingIdx(pcode, 0)
+      while (loop) {
+        addrs.clear()
+        for (i in idx until idx + 20) {
+          val sendAddress = BIP47Util.getInstance(context).getSendAddress(payment_code, i)
+          //                            Log.i("BIP47Activity", "sync send to " + i + ":" + sendAddress.getSendECKey().toAddress(SamouraiWallet.getInstance().getCurrentNetworkParams()).toString());
+//                            BIP47Meta.getInstance().setOutgoingIdx(payment_code.toString(), i);
+          BIP47Meta.getInstance().idx4AddrLookup[BIP47Util.getInstance(context).getSendPubKey(payment_code, i)] = i
+          BIP47Meta.getInstance().pCode4AddrLookup[BIP47Util.getInstance(context).getSendPubKey(payment_code, i)] = payment_code.toString()
+          addrs.add(BIP47Util.getInstance(context).getSendPubKey(payment_code, i))
+        }
+        val s = addrs.toTypedArray()
+        val nb = APIFactory.getInstance(context).syncBIP47Outgoing(s)
+        //                        Log.i("BIP47Activity", "sync send idx:" + idx + ", nb == " + nb);
+        if (nb == 0) {
+          loop = false
+        }
+        idx += 20
+      }
+      BIP47Meta.getInstance().pruneIncoming()
+      PayloadUtil.getInstance(context.applicationContext).saveWalletToJSON(CharSequenceX(AccessFactory.getInstance(context.applicationContext).guid + AccessFactory.getInstance(context.applicationContext).pin))
+    } catch (ioe: IOException) {
+    } catch (je: JSONException) {
+    } catch (de: DecryptionException) {
+    } catch (nse: NotSecp256k1Exception) {
+    } catch (ikse: InvalidKeySpecException) {
+    } catch (ike: InvalidKeyException) {
+    } catch (nsae: NoSuchAlgorithmException) {
+    } catch (nspe: NoSuchProviderException) {
+    } catch (mle: MnemonicException.MnemonicLengthException) {
+    } catch (ex: Exception) {
+    }
+  }
+
+*/
   protected String computeSignature(BIP47Wallet bip47Wallet, String payNymToken) {
     return MessageSignUtilGeneric.getInstance().signMessage(bip47Util.getNotificationAddress(bip47Wallet).getECKey(), payNymToken);
   }
